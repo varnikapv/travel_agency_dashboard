e25e297de85812a0270de637f9056253f4b94748
varnikapv <pvarnika05@gmail.com>
2025-12-16 18:37:59 +0530

    Fix case-sensitive import for Navitems component

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,4 +1,4 @@
-export { default as NavItems } from './NavItems';
+export { default as NavItems } from './Navitems';
 export { default as MobileSidebar } from './MobileSidebar';
 export { default as Header } from './Header';
 export { default as StatsCard } from './StatsCard';
